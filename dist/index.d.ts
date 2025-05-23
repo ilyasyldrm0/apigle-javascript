f8e4e1fa52026b8be148a304ed9bbfe185273bb2
Boztekno <special.3543@gmail.com>
2025-05-23 16:56:52 +0300

    cleanup

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { ApigleClient } from './client';
-declare const client: ApigleClient;
-export default client;
-export { ApigleClient };
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ApigleClient = void 0;
-const client_1 = require("./client");
-Object.defineProperty(exports, "ApigleClient", { enumerable: true, get: function () { return client_1.ApigleClient; } });
-const client = new client_1.ApigleClient();
-exports.default = client;
